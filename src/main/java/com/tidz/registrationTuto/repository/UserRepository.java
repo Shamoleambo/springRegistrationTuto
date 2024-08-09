4c8da4e775cc9773b81447b8d2d742cea73f47d7
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-09 15:35:50 -0300

    fix import

--- src/main/java/com/tidz/registrationTuto/repository/UserRepository.java
+++ src/main/java/com/tidz/registrationTuto/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package com.tidz.registrationTuto.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
+
+import com.tidz.registrationTuto.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
