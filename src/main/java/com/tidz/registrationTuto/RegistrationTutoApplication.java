fe5f0f6e9d1542b431a1d353bd5cc1d80a4a86d4
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-09 15:16:06 -0300

    first commit

---
+++ src/main/java/com/tidz/registrationTuto/RegistrationTutoApplication.java
@@ -0,0 +1,13 @@
+package com.tidz.registrationTuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationTutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tidz/registrationTuto/RegistrationTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidz.registrationTuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
