1c7d9dd698d34bdd174ffa364cdacec34f50df33
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-12 11:29:58 -0300

    add findByEmail method to user repository

--- src/main/java/com/tidz/registrationTuto/repository/UserRepository.java
+++ src/main/java/com/tidz/registrationTuto/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.tidz.registrationTuto.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.tidz.registrationTuto.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+	@Query("SELECT u FROM User u WHERE u.email = ?1")
+	public User findByEmail(String email);
 }
