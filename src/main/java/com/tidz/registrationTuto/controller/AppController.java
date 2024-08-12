20d7d454b63cf92bfe0e16262088224e3334640c
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-12 10:45:43 -0300

    create endpoint to show registration form

--- src/main/java/com/tidz/registrationTuto/controller/AppController.java
+++ src/main/java/com/tidz/registrationTuto/controller/AppController.java
@@ -2,8 +2,10 @@ package com.tidz.registrationTuto.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.tidz.registrationTuto.entity.User;
 import com.tidz.registrationTuto.repository.UserRepository;
 
 @Controller
@@ -15,6 +17,12 @@ public class AppController {
 	@GetMapping("")
 	public String viewHomePage() {
 		return "index";
+	}
+
+	@GetMapping("/register")
+	public String showRegistrationForm(Model model) {
+		model.addAttribute("user", new User());
+		return "signup_form";
 	}
 
 }
